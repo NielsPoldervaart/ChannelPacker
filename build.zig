da79cab4c6a4f7b0bba52b73e6f5eac171c53ec8
Niels Poldervaart <nielspoldervaart@gmail.com>
2026-05-15 15:17:12 +0200

    Fix build.zig

--- build.zig
+++ build.zig
@@ -14,8 +14,10 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "zigimg", .module = zigimg_mod },
+        },
     });
-    lib_mod.addImport("zigimg", zigimg_mod);
 
     const exe = b.addExecutable(.{
         .name = "ChannelPacker",
@@ -23,11 +25,12 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{ .name = "ChannelPacker", .module = lib_mod },
+                .{ .name = "zigimg", .module = zigimg_mod },
+            },
         }),
     });
-
-    exe.root_module.addImport("ChannelPacker", lib_mod);
-    exe.root_module.addImport("zigimg", zigimg_mod);
 
     b.installArtifact(exe);
 
@@ -54,10 +57,16 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
+    const mod_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
     const run_exe_tests = b.addRunArtifact(exe_tests);
     const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 }
